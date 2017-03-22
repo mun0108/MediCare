47f7e192bac80c8c59dfbbfd4e8d0050fbc8e91d
Pallavi <pallavirawat_13122@aitpune.edu.in>
2017-03-22 22:18:08 +0530

    Feature Selection for libsvm data

--- src/DataOperations.py
+++ src/DataOperations.py
@@ -17,7 +17,7 @@ class DataOperations():
     def diabetes_svm_preprocessing(self):
         original_file = open("../data/diabetes_svm.txt", 'r')
         content=original_file.readlines()
-        new_feature_file=open("../data/new_diabetes_svm.txt", 'w')
+        new_feature_file=open("../data/diabetes_svm.txt"+"new.txt", 'w')
         for line in content:
             features=line.split()
             relevant_features=list(features[i] for i in [0, 1, 5, 6, 8])
@@ -25,3 +25,21 @@ class DataOperations():
             new_feature_file.write('\n')
         original_file.close()
         new_feature_file.close()
+
+    def svm_file_feature_select(self,feature_index,file,new_file="new.txt"):
+        original_file = open(file, 'r')
+        content = original_file.readlines()
+        if(new_file=="new.txt"):
+            new_file_name = file + "new.txt"
+        else:
+            new_file_name=new_file
+
+        new_feature_file = open(new_file_name, 'w')
+        for line in content:
+            features = line.split()
+            relevant_features = list(features[i] for i in feature_index)
+            new_feature_file.write(' '.join(relevant_features))
+            new_feature_file.write('\n')
+        original_file.close()
+        new_feature_file.close()
+
--- test/DataOperationsTest.py
+++ test/DataOperationsTest.py
@@ -1,8 +1,19 @@
 from unittest import TestCase
 from src.DataOperations import DataOperations
+import os
 class DataOperationsTest(TestCase):
     def setUp(self):
         self.dataoperations=DataOperations()
 
+    def tearDown(self):
+        if os.path.exists('../data/diabetes_svm_new.txt'):
+            os.remove('../data/diabetes_svm_new.txt')
+        if os.path.exists('../data/diabetes_svm.txtnew.txt'):
+            os.remove('../data/diabetes_svm.txtnew.txt')
+
     def test_diabetes(self):
         self.dataoperations.diabetes_svm_preprocessing()
+
+    def test_feature_selection_new_svm_file(self):
+        self.dataoperations.svm_file_feature_select([0,1,2,3],"../data/diabetes_svm.txt")
+        self.dataoperations.svm_file_feature_select([0,1,2,3,4,5],"../data/diabetes_svm.txt","../data/diabetes_svm_new.txt")
