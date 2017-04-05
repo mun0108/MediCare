2e82ec88b7b33f57cd804acd906ccb57b90713d9
Pallavi <pallavirawat_13122@aitpune.edu.in>
2017-04-05 23:35:31 +0530

    Added saved decision tree models for various datasets

--- test/Classifier_cluster_test_decisionTree.py
+++ test/Classifier_cluster_test_decisionTree.py
@@ -5,17 +5,29 @@ class Classifier_cluster_testDT(TestCase):
         self.classifier_cluster=Classifiers_cluster()
 
     def test_breast_cancer(self):
-        data = "../data/breast_cancer_svm.txt"
-        self.classifier_cluster.Decision_Tree(data)
+        data = "../data/breast_cancer_svm.txtnew.txt"
+        self.classifier_cluster.Decision_Tree(data,"DT_Breast_Cancer_N")
 
     def test_invalid_input(self):
         data="/dsa"
-        self.classifier_cluster.Decision_Tree(data)
+        self.classifier_cluster.Decision_Tree(data,"DT_Dsa")
 
     def test_diabetes(self):
         data="../data/diabetes_svm.txt"
-        self.classifier_cluster.Decision_Tree(data)
+        self.classifier_cluster.Decision_Tree(data,"DT_Diabetes")
 
     def test_diabetes1(self):
-        data="../data/breast_cancer_svm.txt"
-        self.classifier_cluster.Decision_Tree(data)
+        data="../data/diabetes_svm.txtnew.txt"
+        self.classifier_cluster.Decision_Tree(data,"DT_Diabetes_New")
+
+    def test_CKD(self):
+        data="../data/CKD_SVM.txt"
+        self.classifier_cluster.Decision_Tree(data,"DT_CKD")
+
+    def test_fembpsvm(self):
+        data="../data/femBPSVM.txt"
+        self.classifier_cluster.Decision_Tree(data,"DT_femBP")
+
+    def test_heartsvm(self):
+        data="../data/heart_SVM.txt"
+        self.classifier_cluster.Decision_Tree(data,"DT_heart")
--- test/DataOperationsTest.py
+++ test/DataOperationsTest.py
@@ -5,15 +5,15 @@ class DataOperationsTest(TestCase):
     def setUp(self):
         self.dataoperations=DataOperations()
 
-    def tearDown(self):
-        if os.path.exists('../data/diabetes_svm_new.txt'):
-            os.remove('../data/diabetes_svm_new.txt')
-        if os.path.exists('../data/diabetes_svm.txtnew.txt'):
-            os.remove('../data/diabetes_svm.txtnew.txt')
+    # def tearDown(self):
+    #     if os.path.exists('../data/diabetes_svm_new.txt'):
+    #         os.remove('../data/diabetes_svm_new.txt')
+    #     if os.path.exists('../data/diabetes_svm.txtnew.txt'):
+    #         os.remove('../data/diabetes_svm.txtnew.txt')
 
     def test_diabetes(self):
         self.dataoperations.diabetes_svm_preprocessing()
 
     def test_feature_selection_new_svm_file(self):
-        self.dataoperations.svm_file_feature_select([0,1,2,3],"../data/diabetes_svm.txt")
-        self.dataoperations.svm_file_feature_select([0,1,2,3,4,5],"../data/diabetes_svm.txt","../data/diabetes_svm_new.txt")
+        self.dataoperations.svm_file_feature_select([0,2,3,4,5,6,7,8,9,10],"../data/breast_cancer_svm.txt")
+        # self.dataoperations.svm_file_feature_select([0,1,2,3,4,5],"../data/diabetes_svm.txt","../data/diabetes_svm_new.txt")
